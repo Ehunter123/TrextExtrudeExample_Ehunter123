2aafa67b3e2a8e159846793295cb32abd87dd5a3
harrington <harrington@ROBOTICS01.admin.wpi.edu>
2016-12-07 13:36:54 -0500

    changing orentation

--- TrextExtrudeExample.groovy
+++ TrextExtrudeExample.groovy
@@ -1,6 +1,9 @@
 import eu.mihosoft.vrl.v3d.*;
 import java.awt.Font;
 
-Font font = new Font("FreeSerif",  Font.PLAIN, 30);
+Font font = new Font("Harrington",  Font.PLAIN, 30);
 
-return TextExtrude.text((double)10.0,"Hello World 1234!",font)
+return TextExtrude.text((double)10.0,"Hello World 1234!",font).collect{
+	it.rotx(180)
+	.toZMin()
+}
