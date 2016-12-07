ed0554c44a266ce539a46dfc3661e93c6830247e
Kevin Harrington <mad.hephaestus@gmail.com>
2016-12-07 13:05:45 -0500

    Adding text example

--- TrextExtrudeExample.groovy
+++ TrextExtrudeExample.groovy
@@ -1,1 +1,6 @@
-//Your code here
+import eu.mihosoft.vrl.v3d.*;
+import java.awt.Font;
+
+Font font = new Font("FreeSerif",  Font.PLAIN, 30);
+
+return TextExtrude.text((double)10.0,"Hello World 1234!",font)
