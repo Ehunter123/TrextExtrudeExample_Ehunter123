f8dc781f28b7c4538823efef97fec07b37076434
Kevin Harrington <mad.hephaestus@gmail.com>
2023-09-13 11:36:15 -0400

    make a single variable

--- TrextExtrudeExample.groovy
+++ TrextExtrudeExample.groovy
@@ -3,7 +3,11 @@ import javafx.scene.text.Font;
 
 Font font = new Font("Arial",  30);
 
-return TextExtrude.text((double)10.0,"Hello World 1234!",font).collect{
-	it.rotx(180)
-	.toZMin()
-}
+CSG text = CSG.unionAll(
+	TextExtrude.text((double)2,"Hello World! 1 2 3 4 5",font).collect{
+	    it.rotx(180)
+	    .toZMin()
+     }
+)
+
+return text
