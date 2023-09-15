b2e42c581882eaebb17240008426403e078fd287
Kevin harrington <mad.hephaestus@gmail.com>
2023-09-15 14:28:22 -0400

    typo fixed

--- TrextExtrudeExample.groovy
+++ TrextExtrudeExample.groovy
@@ -3,7 +3,7 @@ import eu.mihosoft.vrl.v3d.CSG
 println javafx.scene.text.Font.getFontNames() 
 
 // To use the default Font at 30 point
-CSG text = CSG.text("My Text",5)\
+CSG text = CSG.text("My Text",5)
 
 // Explicatly set the font name and the size
 CSG text2 = CSG.text("Other Font",5.0, 30.0, "FreeSerif")
