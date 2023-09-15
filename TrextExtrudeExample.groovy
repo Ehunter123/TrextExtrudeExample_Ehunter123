c9cfa0bf951dcef68e0c70c853cbb308f6ef66ff
Kevin harrington <mad.hephaestus@gmail.com>
2023-09-15 14:20:47 -0400

    updating the fonts

--- TrextExtrudeExample.groovy
+++ TrextExtrudeExample.groovy
@@ -1,6 +1,12 @@
-import eu.mihosoft.vrl.v3d.*;
-import javafx.scene.text.Font;
+import eu.mihosoft.vrl.v3d.CSG
 
-CSG text = CSG.text("My Text",5)
+println javafx.scene.text.Font.getFontNames() 
 
-return text
+// To use the default Font at 30 point
+CSG text = CSG.text("My Text",5)\
+
+// Explicatly set the font name and the size
+CSG text2 = CSG.text("Other Font",5.0, 30.0, "FreeSerif")
+			.movey(40)
+
+return [text,text2]
