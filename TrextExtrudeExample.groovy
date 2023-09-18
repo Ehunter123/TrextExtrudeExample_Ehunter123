6a1253fafbd6c4a3ef1cecd364860dd3f39e9823
Kevin harrington <mad.hephaestus@gmail.com>
2023-09-18 09:24:39 -0400

    comments

--- TrextExtrudeExample.groovy
+++ TrextExtrudeExample.groovy
@@ -2,7 +2,7 @@ import eu.mihosoft.vrl.v3d.CSG
 
 println javafx.scene.text.Font.getFontNames() 
 
-// To use the default Font at 30 point
+// To use the default Font is Arial at 30 point
 CSG text = CSG.text("My Text",5)
 
 // Explicitly set the font name and the size
